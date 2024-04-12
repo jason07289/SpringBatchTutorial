b32e14bcbed2f4b68bae692c2f9a56de22ba0905
jaeh0728 <jaeh0728@hanwha.com>
2024-04-12 15:08:14 +0900

    parameter validating

---
+++ src/main/java/study/batch/springbatchtutorial/job/HelloWorldJobConfig.java
@@ -0,0 +1,48 @@
+package study.batch.springbatchtutorial.job;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class HelloWorldJobConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job hellowWorldJob() {
+        return jobBuilderFactory.get("helloWorldJob")
+                .incrementer(new RunIdIncrementer())
+                .start(helloWorldStep())
+                .build();
+    }
+
+    @JobScope
+    @Bean
+    public Step helloWorldStep(){
+        return stepBuilderFactory.get("helloWorldStep")
+                .tasklet(helloWorldTasklet())
+                .build();
+    }
+
+    @StepScope
+    @Bean
+    public Tasklet helloWorldTasklet() {
+        return (stepContribution, chunkContext) -> {
+            System.out.println("hello world spring batch");
+            return RepeatStatus.FINISHED;
+        };
+    }
+}
